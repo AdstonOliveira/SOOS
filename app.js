a289b50c92145da240bc314e03e26f1cbdb0f60f
Adston Oliveira <suportetecnico.adston@gmail.com>
2019-09-30 21:21:22 -0300

    teste

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static(__dirname + '/public'));
+app.use( express.static(__dirname + '/public') );
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
