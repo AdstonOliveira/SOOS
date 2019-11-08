70c6fb34a28886aa37f494e9af89786578ba9339
kevin <kevinnatan97@gmail.com>
2019-11-07 22:15:26 -0200

    view nova OS

--- routes/OSRouter.js
+++ routes/OSRouter.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const OSController = require ("../controllers/ControllerOS");
 
 router.get('/nova', OSController.getNovo);
-
+router.get('/novaOS', OSController.getNovaOS);
 module.exports = router;
