f2de8230dac299dcb7a6657ec37ed9daec0b6086
adstonoliveira <suportetecnico.adston@gmail.com>
2019-11-11 21:01:46 -0300

    fazendo login

--- routes/clienteRouter.js
+++ routes/clienteRouter.js
@@ -9,5 +9,6 @@ router.get('/novo', clienteController.getNovo);
 router.get('/testeapi', clienteController.testeAPI);
 
 router.post('/novo', clienteController.postNovo);
+router.post('/login', clienteController.postLogin);
 
 module.exports = router;
