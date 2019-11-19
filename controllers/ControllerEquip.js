0d8154b23181feef9a4d3799879a0c8ff567787b
adstonoliveira <suportetecnico.adston@gmail.com>
2019-11-18 21:53:19 -0300

    view cadastro equipamento

--- controllers/ControllerEquip.js
+++ controllers/ControllerEquip.js
@@ -9,16 +9,25 @@ exports.getTipos = (req, res, next) => {
 }
 
 exports.getNovo = (req,res,next) =>{
+    
 
-    const marcas = knex().select().from('marca')
-        .then( function(result){
-            
-           res.render('equipamento/novo', {marcas: result})
-        }
+    const marcas = knex().select().from('marca').then( function(result){
 
-        ).catch(err=>{
+            const tipo = knex().select().from('tipo').then( function(tipos){
+                console.log('tipo: ' + tipos[0].descricao);
+                console.log('Marca: ' + result[0].marca);
+
+                tipos.forEach(function(tipo){
+                    console.log("ForEach: " + tipo.descricao);
+                })
+                res.render('equipamento/novo',{marcas: result, tipos:tipos});
+            })
+        })       
+        .catch(err=>{
             res.status(500).json({})
         })
+
+        
         
         // console.log(marcas.id)
 }
